6ed927f1dd87df991137a41048d202c8a25c4630
SUMAN RANA <68540296+Suman1999-hub@users.noreply.github.com>
2021-11-25 22:28:31 +0530

    Add files via upload

---
+++ Elephant.java
@@ -0,0 +1,35 @@
+//Elephant
+/*
+ 	An elephant decided to visit his friend. It turned out that the elephant's house is located at point 0 and his friend's house is located at point x(x > 0) of the coordinate line. In one step the elephant can move 1, 2, 3, 4 or 5 positions forward. Determine, what is the minimum number of steps he need to make in order to get to his friend's house.
+
+	Input
+	The first line of the input contains an integer x (1 ≤ x ≤ 1 000 000) — The coordinate of the friend's house.
+	
+	Output
+	Print the minimum number of steps that elephant needs to make to get from point 0 to point x.
+
+Examples
+	input
+	5
+	output
+	1
+	input
+	12
+	output 
+	3
+*/
+import java.util.*;
+public class Elephant {
+		public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			int n=sc.nextInt();
+			int s=n/5;
+			if(n%5==0){
+			    System.out.println(s);
+			}
+			else{
+			    System.out.println(s+1);
+			}
+		}
+	
+}
