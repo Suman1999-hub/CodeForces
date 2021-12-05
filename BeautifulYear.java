56a8a4e8edf424fa1f2e2357f87ed9d03c19dd1d
SUMAN RANA <68540296+Suman1999-hub@users.noreply.github.com>
2021-12-05 13:05:06 +0530

    Add files via upload

---
+++ BeautifulYear.java
@@ -0,0 +1,45 @@
+//Beautiful Year
+/*
+ * input
+		1987
+   output
+        2013
+   input
+        2013
+   output
+        2014
+ */
+import java.util.*;
+public class BeautifulYear {
+	static boolean dist(int year) {
+		String y=Integer.toString(year);
+		char ch[]=y.toCharArray();
+		for(int i=0;i<y.length();i++) {
+			int flag=0;
+			for(int j=0;j<y.length();j++) {
+				if(ch[i]==ch[j]) {
+					flag++;
+					
+				}
+			}
+			if(flag>=2) {
+				return false;
+			}
+		}
+		return true;
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int year=sc.nextInt();
+		year=year+1;
+		while(dist(year)==false) {
+			year++;
+		}
+		System.out.print(year);
+	}
+
+}
