4c64059adf9fcd4446f4122cc578e9c115cd5196
SUMAN RANA <68540296+Suman1999-hub@users.noreply.github.com>
2022-01-11 23:44:05 +0530

    Update KeplersLaw.java

--- KeplersLaw.java
+++ KeplersLaw.java
@@ -1,3 +1,31 @@
+/*
+Kepler’s Law states that the planets move around the sun in elliptical orbits with the sun at one focus. Kepler's 3rd law is The Law of Periods, according to which:
+
+The square of the time period of the planet is directly proportional to the cube of the semimajor axis of its orbit.
+You are given the Time periods (T1,T2) and Semimajor Axes (R1,R2) of two planets orbiting the same star.
+
+Please determine if the Law of Periods is satisfied or not, i.e, if the constant of proportionality of both planets is the same.
+
+Print "Yes" (without quotes) if the law is satisfied, else print "No".
+
+Constraints
+    1≤T≤104
+    1≤T1,T2≤10
+    1≤R1,R2≤10
+Sample Input 1 
+    3
+    1 1 1 1
+    1 2 3 4
+    1 8 2 8
+Sample Output 1 
+    Yes
+    No
+    Yes
+Explanation
+    Test Case 1: 1^2/1^3=1^2/1^3
+    Test Case 2: 1^2/3^3≠2^2/4^3
+    Test Case 3: 1^2/2^3=8^2/8^3
+*/
 package CP;
 import java.util.*;
 public class KeplersLaw {
